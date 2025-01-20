8ad52712322cff1d83da981fdfdf42c278481b25
aarthi <aarthiboominathan@makoitlab.com>
2025-01-20 13:15:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Hello, World! Welcome to my app');
+});
+
+// Route for /admin to test Nginx restriction
+app.get('/admin', (req, res) => {
+  res.send('This is the admin page');
+});
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
